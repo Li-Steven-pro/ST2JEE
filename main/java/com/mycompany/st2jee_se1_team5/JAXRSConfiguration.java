0b819bfeab61e2f93e69e1efe272dce564f22d39
LI Seven <steven.li@efrei.net>
2020-10-23 17:12:48 +0200

    Project Initialization

---
+++ main/java/com/mycompany/st2jee_se1_team5/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.st2jee_se1_team5;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
