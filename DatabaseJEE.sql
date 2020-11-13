389ef75e13276aa1191ad62f226fabf1732a72cb
Medeoha <Medeoha@users.noreply.github.com>
2020-11-13 17:18:05 +0100

    Update DatabaseJEE.sql

--- DatabaseJEE.sql
+++ DatabaseJEE.sql
@@ -17,31 +17,39 @@ CREATE TABLE  visit_sheet  (
 );
 
 CREATE TABLE  mission  (
-  mission_id int PRIMARY KEY AUTO_INCREMENT,
+   mission_id  int PRIMARY KEY AUTO_INCREMENT,
    year  int,
-   visit_sheet_id  int,
    report_title  varchar(255),
-   comments_of_supervisor  text,
    comments_of_the_intern  text,
    mid_internship_meeting_info  text,
    key_word  text,
-   eval_sheet  boolean
-   
+   eval_sheet_id  int,
+   visit_sheet_id  int
 );
 
 CREATE TABLE  teacher  (
-   teacher_id int  PRIMARY KEY AUTO_INCREMENT,
+   teacher_id int PRIMARY KEY AUTO_INCREMENT,
    firstname  varchar(255),
    lastname  varchar(255),
+   login  varchar(255),
    password  varchar(255)
 );
 
 CREATE TABLE  intern  (
-   intern_id int primary key auto_increment,
+   intern_id int PRIMARY KEY AUTO_INCREMENT,
    mission_id  int,
    info_intern_id  int,
    teacher_id  int
 );
+
+CREATE TABLE  eval_sheet  (
+   eval_sheet_id  int PRIMARY KEY AUTO_INCREMENT,
+   comments_of_supervisor  text,
+   grade  int,
+   eval_sheet_done  boolean
+);
+
+ALTER TABLE  mission  ADD FOREIGN KEY ( eval_sheet_id ) REFERENCES  eval_sheet  ( eval_sheet_id );
 
 ALTER TABLE  mission  ADD FOREIGN KEY ( visit_sheet_id ) REFERENCES  visit_sheet  ( visit_sheet_id );
 
@@ -54,8 +62,9 @@ ALTER TABLE  intern  ADD FOREIGN KEY ( teacher_id ) REFERENCES  teacher  ( teach
 INSERT INTO `st2eedb`.`teacher` (`firstname`, `lastname`, `password`) VALUES ('Jean', 'Petit', 'jetit');
 INSERT INTO `st2eedb`.`info_intern` (`firstname`, `lastname`, `skills`, `birthday`) VALUES ('Murielle', 'Bagneux', 'VBA, excel', '1995-10-10');
 INSERT INTO `st2eedb`.`visit_sheet` (`visit_sheet_id`, `visit_planned`, `visit_done`) VALUES ('1', '1', '1');
-INSERT INTO `st2eedb`.`mission` (`mission_id`, `year`, `visit_sheet_id`, `report_title`, `comments_of_supervisor`, `comments_of_the_intern`, `mid_internship_meeting_info`, `key_word`,  `eval_sheet`) VALUES ('1', '2018', '1', 'REPORT_TEST', 'AMAAZING', 'Boring stage', 'yes', 'VBA, EXCEL','1');
-
+INSERT INTO `st2eedb`.`mission` (`mission_id`, `year`, `visit_sheet_id`, `report_title`, `comments_of_the_intern`, `mid_internship_meeting_info`, `key_word`) VALUES ('1', '2018', '1', 'REPORT_TEST', 'Boring stage', 'yes', 'VBA, EXCEL');
+INSERT INTO `st2eedb`.`eval_sheet` (`eval_sheet_id`, `comments_of_supervisor`, `grade`, `eval_sheet_done`) VALUES ('1', 'AMZZING', '20', '1');
+UPDATE `st2eedb`.`mission` SET `eval_sheet_id` = '1' WHERE (`mission_id` = '1');
 INSERT INTO `st2eedb`.`intern` (`intern_id`, `mission_id`, `info_intern_id`, `teacher_id`) VALUES ('1', '1', '1', '1');
 
 
