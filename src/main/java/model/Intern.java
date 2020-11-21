da0c130aaca5d3611c406edc88b5389146e22d7a
Nicolas Guillaume Soulie <soulie.nicolasguillaume@gmail.com>
2020-11-21 17:04:53 +0100

    Clean up useless comments

--- src/main/java/model/Intern.java
+++ src/main/java/model/Intern.java
@@ -142,8 +142,6 @@ public class Intern {
         return address;
     }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
     public void setAddress(String address) {
         this.address = address;
     }
